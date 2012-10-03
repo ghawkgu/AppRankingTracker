c15a630da3c26fd261eec16b40a3c515d833e9f0
Yi Gu <ghawk.gu@gmail.com>
2012-10-03 16:08:39 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppRankingTracker::Application.config.secret_token = 'e3fd8c07e677f86418d92521d20d86dd06a1577c5be863190f9edba7a98f7a1d99f71315be7766fd12ba27180e0784af691af1d18681e444b1bcc448ccef6ced'
