59675dde0b2c270e9979c1a81a7f040668c5b652
Yi Gu <guyi@ngigroup.com>
2012-10-09 16:41:42 +0900

    Add description to the category model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,10 @@ class Category < ActiveRecord::Base
   attr_accessible :id, :label, :sub_categories, :parent
   has_many :sub_categories, :class_name => 'Category', :primary_key => 'id', :foreign_key => 'parent_id'
   belongs_to :parent, :class_name => 'Category', :primary_key => 'id', :foreign_key => 'parent_id'
+
+  def to_s
+    desc = ""
+    desc += "#{parent.label}(#{parent.id}) -> " if parent
+    desc += "#{label}(#{id})"
+  end
 end
